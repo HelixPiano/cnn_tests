27ed91d69b870b797c511d3d1b449d4d20eb8f9c
HelixPiano <62934678+HelixPiano@users.noreply.github.com>
2024-08-09 14:59:48 +0200

    runs evolution

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import training
+import evolve
 import test_run_function
 
 if __name__ == '__main__':
-    test_run_function.run_function_from_script()
+    evolve.run_evolution()
 # tensorboard --logdir="lightning_logs"
 
-#TODO: Modify evolve/fitness to run in one go
 #TODO: modify indi0001_GWL.py to run
 #TODO: change template to run GWL
