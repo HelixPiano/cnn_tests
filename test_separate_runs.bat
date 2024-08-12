1f2a94078d862de9e4107e3bbd3a606bcb6c61f6
HelixPiano <62934678+HelixPiano@users.noreply.github.com>
2024-08-12 11:55:40 +0200

    updates

---
+++ test_separate_runs.bat
@@ -0,0 +1,15 @@
+call conda activate cnn_gpu
+echo "Starting Generation 0"
+python evolve.py
+echo "Starting Generation 1"
+python evolve.py
+echo "Starting Generation 2"
+python evolve.py
+echo "Starting Generation 3"
+python evolve.py
+echo "Starting Generation 4"
+python evolve.py
+echo "Done?"
+echo "Starting Generation 5"
+python evolve.py
+pause
